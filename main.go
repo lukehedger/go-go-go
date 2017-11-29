c8110cfd2b7950c9f9a2f08849d902f7ae278568
lukehedger <luke.hedger@gmail.com>
2017-11-29 12:18:07 +0000

    Updated

--- main.go
+++ main.go
@@ -1,12 +1,47 @@
 // Executable commands must always use `package main`
 package main
 
+// 'factored' import statements can be used to group individual imports
 import (
-	// "fmt"
+	"fmt"
 	"github.com/lukehedger/golib"
 )
 
 func main() {
-	// fmt.Printf("Go, Go, Go!\n")
-	golib.Echo(golib.Reverse("\n!oG, oG, oG"))
+	fmt.Printf(":: Reverse ::\n")
+	golib.Echo(golib.Reverse("\n!oG ,oG ,oG"))
+	fmt.Printf("\n")
+
+	fmt.Printf(":: Concat ::\n")
+	a, b := "Hello", "World"
+	golib.Echo(golib.Concat(a, b))
+	fmt.Printf("\n")
+
+	fmt.Printf(":: Conditioner ::\n")
+	golib.Conditioner(9)
+	fmt.Printf("\n")
+
+	fmt.Printf(":: Looper ::\n")
+	golib.Looper()
+	fmt.Printf("\n")
+
+	fmt.Printf(":: Pointers ::\n")
+	golib.Pointers()
+	fmt.Printf("\n")
+
+	fmt.Printf(":: Structs ::\n")
+	golib.Structs()
+	fmt.Printf("\n")
+
+	fmt.Printf(":: Swap ::\n")
+	fmt.Println(golib.Swap(a, b))
+	fmt.Printf("\n")
+
+	fmt.Printf(":: Switcheroo ::\n")
+	golib.Switcheroo()
+	fmt.Printf("\n")
+
+	fmt.Printf(":: Variables ::\n")
+	golib.Variables()
+	fmt.Printf("\n")
 }
